dab6a5f00367999fac391e398c6129d0b8c8f773
wanning <z5371162@unsw.edu.au>
2022-12-24 20:27:16 +1100

    set up google user authentication

--- src/Components/StartButton.jsx
+++ src/Components/StartButton.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { auth, provider } from '../Firebase';
+import { signInWithPopup } from 'firebase/auth';
 
-const StartButton = () => {
+function StartButton() {
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      const credential = provider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log('user', user).catch((error) => {
+        console.log(error.message);
+      });
+    });
+  };
   return (
-    <Button variant='outlined' size='large' color='secondary'>
+    <Button
+      variant='outlined'
+      size='large'
+      color='secondary'
+      onClick={handleClick}
+    >
       Get Started!
     </Button>
   );
-};
+}
 
 export default StartButton;
--- src/Firebase.js
+++ src/Firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,3 +23,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { auth, provider };
